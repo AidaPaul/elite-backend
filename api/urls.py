fbd10392e317e5c359d001f0093d63d2d3e40a44
Tymoteusz Paul <tymoteusz.paul@x20x.co.uk>
2015-03-26 13:33:40 +0000

    For now let the root point to API

--- api/urls.py
+++ api/urls.py
@@ -21,6 +21,6 @@ router.register(r'listings', views.ListingViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^api/', include(router.urls)),
+    url(r'^/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
