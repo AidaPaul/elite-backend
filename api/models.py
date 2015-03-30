e5cf89e88bb11972edc7ad128fa1e8011a6d7633
Tymoteusz Paul <tymoteusz.paul@x20x.co.uk>
2015-03-30 10:52:52 +0100

    String representation for reamining models to make admin tools easier to use.

--- api/models.py
+++ api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Commodity(models.Model):
     category = models.ForeignKey(Category)
@@ -115,7 +118,7 @@ class Station(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return "%s > %s" % (self.system, self.name)
 
 
 class Listing(models.Model):
@@ -127,6 +130,9 @@ class Listing(models.Model):
     sell_price = models.IntegerField()
     collected_at = models.DateTimeField()
     update_count = models.IntegerField()
+
+    def __str__(self):
+        return "%s > %s" % (self.station, self.commodity)
 
 
 class AllegianceRank(models.Model):
